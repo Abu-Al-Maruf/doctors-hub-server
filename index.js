2b939d5f910e8c61459f9de710d2819d474ea2cf
Abu Al Maruf <mdmaruf55667711@gmail.com>
2025-02-01 01:28:12 +0600

    implement verifyAdmin and delete a user route

--- index.js
+++ index.js
@@ -31,12 +31,12 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
 
-    const userCollection = client.db("doctors_hub").collection("users");
-    const doctorCollection = client.db("doctors_hub").collection("doctors");
-    const reviewCollection = client.db("doctors_hub").collection("reviews");
-    const serviceCollection = client.db("doctors_hub").collection("services");
+    const usersCollection = client.db("doctors_hub").collection("users");
+    const doctorsCollection = client.db("doctors_hub").collection("doctors");
+    const reviewsCollection = client.db("doctors_hub").collection("reviews");
+    const servicesCollection = client.db("doctors_hub").collection("services");
 
-    // middleware
+    // middlewares -------------------------------- start
     const verifyToken = (req, res, next) => {
       if (!req.headers.authorization) {
         return res.status(401).send({ message: "unauthorize access" });
@@ -51,6 +51,18 @@ async function run() {
         next();
       });
     };
+    // verify admin
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      if (user?.role !== "Admin") {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+      next();
+    };
+
+    // middlewares -------------------------------- end
 
     // ------ jwt related api --------->
     app.post("/jwt", async (req, res) => {
@@ -62,46 +74,68 @@ async function run() {
     });
 
     // ------ users related api --------->
-    app.get("/users", verifyToken, async (req, res) => {
-      console.log(req.decoded);
-      const result = await userCollection.find().toArray();
+    app.get("/users", verifyToken, verifyAdmin, async (req, res) => {
+      const result = await usersCollection.find().toArray();
       res.send(result);
+    });
+
+    // check is admin
+    app.get("/users/admin/:email", verifyToken, async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+      const user = await usersCollection.findOne(query);
+      let admin = false;
+      if (user?.role === "Admin") {
+        admin = true;
+      }
+      res.send({ admin });
     });
 
     app.post("/user", async (req, res) => {
       const user = req.body;
-      const existingUser = await userCollection.findOne({ email: user.email });
+      const existingUser = await usersCollection.findOne({ email: user.email });
       if (existingUser) {
         return res.send({
           message: "user already exists",
           insertedId: null,
         });
       }
-      const result = await userCollection.insertOne(user);
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    // delete a user
+    app.delete("/user/:id", verifyToken, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await usersCollection.deleteOne(query);
       res.send(result);
     });
 
     //------  doctors related api --------->
     app.get("/doctors", async (req, res) => {
-      const result = await doctorCollection.find().toArray();
+      const result = await doctorsCollection.find().toArray();
       res.send(result);
     });
     app.get("/doctors/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
-      const result = await doctorCollection.findOne(query);
+      const result = await doctorsCollection.findOne(query);
       res.send(result);
     });
 
     // ------ reviews related api ---------->
     app.get("/reviews", async (req, res) => {
-      const result = await reviewCollection.find().toArray();
+      const result = await reviewsCollection.find().toArray();
       res.send(result);
     });
 
     //------  services related api --------->
     app.get("/services", async (req, res) => {
-      const result = await serviceCollection.find().toArray();
+      const result = await servicesCollection.find().toArray();
       res.send(result);
     });
 
